88ea9727605eff5043359d3f3512c6922d56524a
bouzagui <badrbouzagui@gmail.com>
2024-07-26 13:58:43 +0100

    done ✅

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,22 +7,26 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def home():
+    """Home page"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """hbnb page"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def cisfun(text):
+    """text of C"""
     text.replace("_", " ")
     return f"C {text}"
 
 
 @app.route("/python/<text>", strict_slashes=False)
 def pyiscool(text):
+    """text of python"""
     text.replace("_", " ")
     return f"Python {text}"
 
