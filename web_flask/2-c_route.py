218c6d45b5b79cd7e42ad052589952a93097783a
bouzagui <badrbouzagui@gmail.com>
2024-07-26 13:23:45 +0100

    done ✅

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,18 +4,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def home_page():
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def cisfun(text):
     text = text.replace(" ", "_")
     return f"C {text}"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
