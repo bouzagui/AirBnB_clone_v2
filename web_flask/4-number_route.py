aecd8554559c2c272cc6af6109fb47c4c7c55dbd
bouzagui <badrbouzagui@gmail.com>
2024-07-26 15:52:06 +0100

    done ✅

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -2,35 +2,37 @@
 """models"""
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def home():
-    """Home page"""
-    return "Hello HBNB!"
+    return "Hello HBNB"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """hbnb page"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def cisfun(text):
-    """text of C"""
     text = text.replace("_", " ")
     return f"C {text}"
 
 
-@app.route("/python/", strict_slashes=False)
+@app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def pyiscool(text="is cool"):
-    """is cool"""
     text = text.replace("_", " ")
     return f"Python {text}"
 
 
+@app.route("/number/<int:n>", strict_slashes=False)
+def num(n):
+    return f"{n} is a number"
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
