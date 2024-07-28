d84bb6b5cf73d0bc600c58877ecc03b92ca5631d
bouzagui <badrbouzagui@gmail.com>
2024-07-28 12:35:52 +0100

    done ✅

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,14 +15,14 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage,
-        optionally filtered by class."""
-        if cls:
-            if isinstance(cls, str):
-                cls = eval(cls)
-            filtered_objects = {k: v for k, v in self.__objects.items()
-                                if isinstance(v, cls)}
-        return filtered_objects
+        """returns the dictionary __objects"""
+        if cls is not None:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    new_dict[key] = value
+            return new_dict
+        return self.__objects
 
     def new(self, obj):
         """Adds a new object to the storage dictionary."""
